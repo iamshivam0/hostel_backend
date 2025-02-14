5434a0fe26759fca5733deb5169726efee7ea79f
Vinay <101291613+Ayush4221@users.noreply.github.com>
2025-02-15 00:23:01 +0530

    major bug fixes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -93,6 +93,7 @@ export const register = async (req: Request, res: Response) => {
       userData.location = location;
     } else {
       userData.location = undefined;
+      // location.type === "Point"
     }
     // Add role-specific fields ONLY
     switch (role) {
@@ -115,7 +116,9 @@ export const register = async (req: Request, res: Response) => {
         userData.children = undefined;
         userData.parentId = undefined;
         userData.roomNumber = undefined;
-        userData.lo
+        break;
+        userData.location = undefined;
+      // location.type = "Point";
     }
 
     const user = new User(userData);
@@ -160,7 +163,7 @@ export const requestPasswordReset = async (req: Request, res: Response) => {
   try {
     const { email } = req.body;
     const { resetToken, user } = await generateResetToken(email);
-    const resetLink = `https://hostel-frontend-fx5j.vercel.app/reset-password?token=${resetToken}`;
+    const resetLink = `${process.env.FRONTEND_LINK}/reset-password?token=${resetToken}`;
     await sendResetPasswordEmail(user.email);
     res.status(200).json({ message: "Password reset email sent" });
   } catch (error: any) {
